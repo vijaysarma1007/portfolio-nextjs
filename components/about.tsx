366c3094433d0f33dacad7e990ca05f23ea0d755
Vijay Mohan Srirangam <104432407+vijaysarma1007@users.noreply.github.com>
2024-05-18 21:07:39 +0530

    Update about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -18,7 +18,7 @@ export default function About() {
       <SectionHeading>About Me</SectionHeading>
       <p className='mb-3'>
         After graduating with a degree in{' '}
-        <span className='font-medium'>Accounting</span>, I decided to pursue my
+        <span className='font-medium'>Engineering and Management</span>, I decided to pursue my
         passion for programming. I enrolled in a coding bootcamp and learned{' '}
         <span className='font-medium'>full-stack web development</span>.{' '}
         <span className='italic'>My favorite part of programming</span> is the
@@ -28,19 +28,19 @@ export default function About() {
         <span className='font-medium'>
           React, Next.js, Node.js, and MongoDB
         </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{' '}
-        <span className='font-medium'>full-time position</span> as a software
+        . I am also familiar with TypeScript with Micro Servics. I am always looking to
+        learn new technologies. I am currently learning in the field of{' '}
+        <span className='font-medium'>Artificial Intelligence and Machine Learning</span> as a software
         developer.
       </p>
 
       <p>
         <span className='italic'>When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{' '}
+        video games, watching movie. I also enjoy{' '}
         <span className='font-medium'>learning new things</span>. I am currently
         learning about{' '}
-        <span className='font-medium'>history and philosophy</span>. I'm also
-        learning how to play the guitar.
+        <span className='font-medium'>history and politics</span>. I'm also
+        learning content writing.
       </p>
     </motion.section>
   );
